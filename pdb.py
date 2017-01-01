108fae451f2c2c174a56abeec5d949d91d0df68e
Jason Jones <jason.jones.programmer@gmail.com>
2016-12-31 18:09:54 -0700

    Added code to help store breakpoints in a file and set breakpoints using that file.

--- pdb.py
+++ pdb.py
@@ -125,7 +125,7 @@ def rebind_globals(func, newglobals=None):
 class DefaultConfig:
     prompt = '(Pdb++) '
     highlight = True
-    sticky_by_default = False
+    sticky_by_default = True
     bg = 'dark'
     use_pygments = True
     colorscheme = None
@@ -147,7 +147,27 @@ class DefaultConfig:
         pass
 
     def before_interaction_hook(self, pdb):
-        pass
+        self.setup_breakpoints(pdb)
+
+    def setup_breakpoints(self, pdb):
+        """Changes to help store breakpoints in a file. pdb.set_trace() should be
+        called at the beginning of the entry script so that the breakpoints are
+        loaded.
+        """
+        breakpoints_path = os.path.expanduser('~') + '/.py-breakpoints'
+        if not os.path.exists(breakpoints_path):
+            pdb.stdout.write(".py-breakpoints not found in home directory")
+            return
+
+        with open(breakpoints_path) as bfile:
+            breakpoints = bfile.read().split("\n")
+
+        for breakpoint in breakpoints:
+            if not breakpoint:
+                continue
+            pdb.stdout.write("Setting breakpoint: {}\n".format(breakpoint))
+            pdb._set_moving_break(breakpoint)
+
 
 def setbgcolor(line, color):
     # hack hack hack
@@ -426,6 +446,134 @@ class Pdb(pdb.Pdb, ConfigurableClass):
             line = '!' + line
             return pdb.Pdb.parseline(self, line)
         return cmd, arg, newline
+
+    def _checkline_moving(self, filename, lineno):
+        """Check whether specified line seems to be executable.
+
+        Return `lineno` if it is, 0 if not (e.g. a docstring, comment, blank
+        line or EOF). Warning: testing is not comprehensive.
+
+        This is mostly copied from the checkline() function in the base pdb.py that accompanied Python 3.4.
+        """
+        # this method should be callable before starting debugging, so default
+        # to "no globals" if there is no current frame
+        globs = self.curframe.f_globals if hasattr(self, 'curframe') else None
+        import linecache
+        line = linecache.getline(filename, lineno, globs)
+        if not line:
+            self.message('End of file')
+            return 0
+        line = line.strip()
+        # Don't allow setting breakpoint at a blank line
+        if (not line or (line[0] == '#') or
+             (line[:3] == '"""') or line[:3] == "'''"):
+            self.error('Blank or comment')
+            return -1
+        return lineno
+
+    def _set_moving_break(self, arg, temporary = 0):
+        """Attempt to set a breakpoint, and move the breakpoint if it's not on
+        a valid line.
+
+        b(reak) [ ([filename:]lineno | function) [, condition] ]
+        Without argument, list all breaks.
+
+        With a line number argument, set a break at this line in the
+        current file.  With a function name, set a break at the first
+        executable line of that function.  If a second argument is
+        present, it is a string specifying an expression which must
+        evaluate to true before the breakpoint is honored.
+
+        The line number may be prefixed with a filename and a colon,
+        to specify a breakpoint in another file (probably one that
+        hasn't been loaded yet).  The file is searched for on
+        sys.path; the .py suffix may be omitted.
+
+        This is mostly copied from the do_break() function in the base pdb.py that accompanied Python 3.4.
+        """
+        if not arg:
+            if self.breaks:  # There's at least one
+                self.message("Num Type         Disp Enb   Where")
+                for bp in bdb.Breakpoint.bpbynumber:
+                    if bp:
+                        self.message(bp.bpformat())
+            return
+        # parse arguments; comma has lowest precedence
+        # and cannot occur in filename
+        filename = None
+        lineno = None
+        cond = None
+        comma = arg.find(',')
+        if comma > 0:
+            # parse stuff after comma: "condition"
+            cond = arg[comma+1:].lstrip()
+            arg = arg[:comma].rstrip()
+        # parse stuff before comma: [filename:]lineno | function
+        colon = arg.rfind(':')
+        funcname = None
+        if colon >= 0:
+            filename = arg[:colon].rstrip()
+            f = self.lookupmodule(filename)
+            if not f:
+                self.error('%r not found from sys.path' % filename)
+                return
+            else:
+                filename = f
+            arg = arg[colon+1:].lstrip()
+            try:
+                lineno = int(arg)
+            except ValueError:
+                self.error('Bad lineno: %s' % arg)
+                return
+        else:
+            # no colon; can be lineno or function
+            try:
+                lineno = int(arg)
+            except ValueError:
+                try:
+                    func = eval(arg,
+                                self.curframe.f_globals,
+                                self.curframe_locals)
+                except:
+                    func = arg
+                try:
+                    if hasattr(func, '__func__'):
+                        func = func.__func__
+                    code = func.__code__
+                    #use co_name to identify the bkpt (function names
+                    #could be aliased, but co_name is invariant)
+                    funcname = code.co_name
+                    lineno = code.co_firstlineno
+                    filename = code.co_filename
+                except:
+                    # last thing to try
+                    (ok, filename, ln) = self.lineinfo(arg)
+                    if not ok:
+                        self.error('The specified object %r is not a function '
+                                   'or was not found along sys.path.' % arg)
+                        return
+                    funcname = ok # ok contains a function name
+                    lineno = int(ln)
+        if not filename:
+            filename = self.defaultFile()
+
+        # If the breakpoint is not on a valid line, then go to the next valid line
+        while 1:
+            line = self._checkline_moving(filename, lineno)
+            if line > 0:
+                # if we've hit the end of the file, or found an executable line, use that line
+                break
+            lineno += 1
+
+        if line:
+            # now set the break point
+            err = self.set_break(filename, line, temporary, cond, funcname)
+            if err:
+                self.error(err)
+            else:
+                bp = self.get_breaks(filename, line)[-1]
+                self.message("Breakpoint %d at %s:%d" %
+                             (bp.number, bp.file, bp.line))
 
     def do_inspect(self, arg):
         obj = self._getval(arg)
